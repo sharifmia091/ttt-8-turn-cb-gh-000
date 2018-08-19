439821db6f0ca67bb315bbdec535fe7f41e1b280
md mia <sharifmia091@gmail.com>
2018-08-19 11:34:35 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -50,7 +50,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
 
-  if (valid_move?(board, index)==true)
+  if valid_move?(board, index)==true
     move(board,index)
     display_board(board)
 
@@ -58,5 +58,4 @@ def turn(board)
     turn(board)
   end
 
-end
-end
+
