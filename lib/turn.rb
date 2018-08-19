f26b589ec684b29a58f75e440bf13361c2c33895
md mia <sharifmia091@gmail.com>
2018-08-19 10:48:27 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,25 +6,22 @@ def display_board(board)
   puts" #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+  input = input.to_i
+  input-=1
+  return input
+end
+
+
+def move(board,index,character ="X")
+  board[index] = character
+  return board
+end
+
 def turn(board)
   puts "Where would you like to go?"
   puts "Please enter 1-9 to "
   input = gets.strip
-
-  def input_to_index(input)
-    input = input.to_i
-    input-=1
-    return input
-  end
-
   index = input_to_index(input)
-
-  def move(board,index,character ="X")
-    board[index] = character
-    return board
-  end
-  board = [" "," "," "," "," "," "," "," "," "]
-  display_board(board)
-
 
 end
