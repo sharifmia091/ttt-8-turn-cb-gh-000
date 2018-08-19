c54859305ddfb14af23c60772199c9f71986937d
md mia <sharifmia091@gmail.com>
2018-08-19 10:29:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,9 +6,17 @@ def display_board(board)
   puts"   |   |   "
 end
 
+
+def input_to_index(input)
+  input = input.to_i
+  input-=1
+  return input
+end
+
 def turn(board)
   puts "Where would you like to go?"
   puts "Please enter 1-9 to "
-  
+  input = gets.strip
+  index = input_to_index(input)
 
 end
