e54ec10c045992e51f44a59ab6196b430ffc24ed
md mia <sharifmia091@gmail.com>
2018-08-19 11:45:17 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,8 +6,8 @@ def display_board(board)
   puts" #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-  input = input.to_i
+def input_to_index(user_input)
+  input = user_input.to_i
   input-=1
   return input
 end
